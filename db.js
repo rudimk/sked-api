ffdf7c29eccf0982a73930e9732080d7ab16e33b
Rudraksh MK <rudimk.cloud@gmail.com>
2018-05-15 15:00:34 +0530

    Added a module for creating a MySQL connection pool.

---
+++ db.js
@@ -0,0 +1,9 @@
+exports.knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host : process.env.DB_HOST,
+    user : process.env.DB_USER,
+    password : process.env.DB_PASSWD,
+    database : process.env.DB_NAME
+  }
+})
