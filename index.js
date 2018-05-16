4dac754d45eb6f79f96ddbd36f80a9f1af7f2db8
Rudraksh MK <rudimk.cloud@gmail.com>
2018-05-16 10:31:05 +0530

    Added code to instantiate an Express app.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 var express = require("express")
 var bodyParser = require("body-parser")
 var controllers = require("./controllers.js")
+
+var app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+controllers(app)
+
+var server = app.listen(8080, '0.0.0.0', function () {
+    console.log("The Sked API is listening on port.", server.address().port);
+})
