cb6f31197840efc70a607c45b79e77c674e2ef4c
Rudraksh MK <rudimk.cloud@gmail.com>
2018-05-16 10:52:16 +0530

    Initialized API, added an endpoint for health checks.

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const server = require("diet")
 const requestLog = require('./middleware/requestLog.js')
 const healthCheck = require('./middleware/healthCheck.js')
+
+const app = server()
+app.listen('http://0.0.0.0:8080')
+
+// Adding a header for logging all incoming requests.
+app.header(requestLog)
+
+// Endpoint for health checks
+app.get('/api/v2/status', healthCheck.healthCheck)
