e766fce707a848588f2f55fce87099e492e951e2
Rudraksh MK <rudimk.cloud@gmail.com>
2018-05-16 10:43:38 +0530

    Added a middleware function for logging every request.

---
+++ middleware/request-log.js
@@ -0,0 +1,11 @@
+const logging = require('../logging.js')
+
+// We're defining a middleware function here for logging the request timestamp, method, and url
+// TODO figure out a better way to log this
+
+async function logRequest($){
+	logging.winston.info(new Date().toISOString() + ": " + $.method + ": " + $.url.path)
+	$.return()
+}
+
+module.exports = logRequest
