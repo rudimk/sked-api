d995713cbc04288fddcd0b0ab73f6c9bf250785a
Rudraksh MK <rudimk.cloud@gmail.com>
2018-05-21 03:07:15 +0530

    Added a controller and an endpoint for adding a new schedule and its associated AMQP workflow.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const server = require("diet")
 const requestLog = require('./middleware/requestLog.js')
 const healthCheck = require('./middleware/healthCheck.js')
+const controllers = require('./controllers.js')
 
 const app = server()
 app.listen('http://0.0.0.0:8080')
@@ -10,3 +11,6 @@ app.header(requestLog)
 
 // Endpoint for health checks
 app.get('/api/v2/status', healthCheck.healthCheck)
+
+// Endpoint for adding a new schedule and associated workflow.
+app.post('/api/v2/schedule', controllers.newScheduleController)
