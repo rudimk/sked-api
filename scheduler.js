0b51870985d68ebd72f1415ab6a241dda1aab521
Rudraksh MK <rudimk.cloud@gmail.com>
2018-05-28 16:40:14 +0530

    The scheduler.startRunner function now returns the Rancher service ID.

--- scheduler.js
+++ scheduler.js
@@ -4,8 +4,8 @@ const logger = require('./logging.js')
 async function startRunner(payload){
 	const cattleRustler = new rancher.Rancher(process.env.RANCHER_URL, process.env.RANCHER_API_KEY, process.env.RANCHER_API_SECRET)
 	let newRunnerService = await cattleRustler.createStackService(process.env.RANCHER_ENV_ID, process.env.RANCHER_STACK_ID, payload.serviceName, payload.labels, payload.environmentVars, process.env.RUNNER_IMAGE_ID)
-	logger.winston.info(`Scheduled new runner: ${newRunnerService}`)
-	return true
+	logger.winston.info(`Scheduled new runner: ${newRunnerService['id']}`)
+	return newRunnerService['id']
 }
 
 exports.startRunner = startRunner
