42b0dc0b96c6a774bffb14194913681ba0a50a44
Rudraksh MK <rudimk.cloud@gmail.com>
2018-05-29 14:11:50 +0530

    Modified the API to start/stop schedule runners on Rancher as and when appropriate.

--- controllers.js
+++ controllers.js
@@ -28,7 +28,7 @@ async function newScheduleController($){
 		runnerPayload['environmentVars'] = {DB_HOST: process.env.DB_HOST, DB_USER: process.env.DB_USER, DB_PASSWD: process.env.DB_PASSWD, DB_NAME: process.env.DB_NAME, AMQP_USERNAME: workflowPayload['username'], AMQP_PASSWORD: workflowPayload['password'], AMQP_HOST: workflowPayload['host'], AMQP_PORT: workflowPayload['port'], AMQP_EXCHANGE: workflowPayload['exchange'], AMQP_ROUTING_KEY: workflowPayload['routing_key'], AMQP_PAYLOAD: util.format('%j', workflowPayload['payload']), TIMEZONE: schedulePayload['timezone'], MINUTES: schedulePayload['minutes'], HOURS: schedulePayload['hours'], WEEKDAYS: schedulePayload['weekdays'], DAYS: schedulePayload['days'], MONTHS: schedulePayload['months'], SCHEDULE_ID: newScheduleRow[0], WORKFLOW_ID: newWorkflowRow[0]}
 		runnerPayload['serviceName'] = schedulePayload['runner_id']
 		runnerPayload['scale'] = parseInt(process.env.SCALE)
-		let scheduleRunner = await scheduler.startRunner(runnerPayload)
+		let scheduleRunner = await scheduler.createRunner(runnerPayload)
 		let updateScheduleRow = await db('schedules').where('id', '=', newScheduleRow[0]).update({service_id: scheduleRunner})
 		$.status(200)
 		$.json({success: true, message: 'Schedule created successfully.', schedule_id: newScheduleRow[0], workflow_id: newWorkflowRow[0], runner_id: scheduleRunner['id']})
@@ -44,10 +44,12 @@ async function newScheduleController($){
 async function updateScheduleController($){
 	try{
 		let deactivateSchedule = await db('schedules').where('id', '=', $.query['id']).update({active: 0})
+		let runner = await db.select('service_id').from('schedules').where('id', '=', $.query['id'])
+		let stopScheduleRunner = await scheduler.stopRunner(runner[0]['service_id'])
 		let rawPayload = JSON.parse($.body)
 		let schedulePayload = rawPayload['schedule']
 		let workflowPayload = rawPayload['workflow']
-		schedulePayload['status'] = 0
+		schedulePayload['status'] = 1
 		schedulePayload['active'] = 1
 		schedulePayload['minutes'] = JSON.stringify(schedulePayload['minutes'])
 		schedulePayload['hours'] = JSON.stringify(schedulePayload['hours'])
@@ -73,6 +75,14 @@ async function updateScheduleController($){
 async function cycleScheduleController($){
 	try{
 		let cycleSchedule = await db('schedules').where('id', '=', $.query['id']).update({active: $.query['active']})
+		if (parseInt($.query['active']) == 0){
+			let runner = await db.select('service_id').from('schedules').where('id', '=', $.query['id'])
+			let stopScheduleRunner = await scheduler.stopRunner(runner[0]['service_id'])
+		}
+		else{
+			let runner = await db.select('service_id').from('schedules').where('id', '=', $.query['id'])
+			let startScheduleRunner = await scheduler.startRunner(runner[0]['service_id'])
+		}
 		$.status(200)
 		$.json({success: true, message: `Schedule status successfully set to ${$.query['active']}.`})
 	}
