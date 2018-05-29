17e803a55770ebe096870421bf9bd274b2fdffaf
Rudraksh MK <rudimk.cloud@gmail.com>
2018-05-29 11:36:01 +0530

    Added the scale parameter to the newScheduleController controller and the startRunner function.

--- scheduler.js
+++ scheduler.js
@@ -3,7 +3,7 @@ const logger = require('./logging.js')
 
 async function startRunner(payload){
 	const cattleRustler = new rancher.Rancher(process.env.RANCHER_URL, process.env.RANCHER_API_KEY, process.env.RANCHER_API_SECRET)
-	let newRunnerService = await cattleRustler.createStackService(process.env.RANCHER_ENV_ID, process.env.RANCHER_STACK_ID, payload.serviceName, payload.labels, payload.environmentVars, process.env.RUNNER_IMAGE_ID)
+	let newRunnerService = await cattleRustler.createStackService(process.env.RANCHER_ENV_ID, process.env.RANCHER_STACK_ID, payload.scale, payload.serviceName, payload.labels, payload.environmentVars, process.env.RUNNER_IMAGE_ID)
 	logger.winston.info(`Scheduled new runner: ${newRunnerService['id']}`)
 	return newRunnerService['id']
 }
